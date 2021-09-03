7d4e57af4fa27d21c957d743046bab2086d92928
NatsukiYu <tsgr.natsuki.yu@gmail.com>
2021-09-03 16:33:26 +0900

    create setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name='PingCog',
+    version='1.0.0',
+    license='MIT',
+    description='Slash command example',
+    author='@natsuki__yu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=requirements,
+)
